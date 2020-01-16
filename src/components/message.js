a37ca89687010d239f80b7dc0bda3a2a6267b133
Mariola <turowska.mariola@gmail.com>
2020-01-16 21:05:14 +0100

    hooks, context api added

--- src/components/message.js
+++ src/components/message.js
@@ -3,6 +3,7 @@ import {addMessage, broadcastMessage} from '../api';
 import Color from "./color";
 import {ColorContext} from '../contexts/colorContext';
 import CanvasComponent from "./canvas";
+import {IoMdBrush,IoIosSend} from "react-icons/io";
 
 let moment = require('moment');
 const Message = () => {
@@ -33,26 +34,32 @@ const Message = () => {
                         <div className ='messages-area'>
                             {!!messages && messages.map((e, i) => {
                                 return (
-                                    <div key={i}>
-                                        <p>{e.time}</p>
-                                        <p style={{color: e.color}}>{e.text}</p>
+                                    <div key={i} className='messages-area-one'>
+                                        <p className ='message-one-time'>{e.time}</p>
+                                        <p style={{color: e.color}} className='message-one-text'>{e.text}</p>
                                     </div>)
                             })}
                         </div>
-                        <div>
+                        <div className='screen-area'>
+                        <div className='buttons-color-area'>
                             {colors.map((color, i) => {
                                 return (
                                     <Color key={i} color={color}/>
                                 )
                             })
                             }
+                            <IoMdBrush size='1.5em' color='grey'/>
                         </div>
                         <input value={singleMes}
                                placeholder="Type your message here..."
                                style={{color: fontColor}}
-                               onChange={event => setSingleMes(event.target.value)}/>
+                               className='input-styling'
+                               onChange={event => setSingleMes(event.target.value)}>
+
+                        </input>
+                            <IoIosSend onClick={() => clickHandler(fontColor)} color='grey' size='1.5em'/>
                         <CanvasComponent/>
-                        <button onClick={() => clickHandler(fontColor)}>Submit</button>
+                        </div>
                     </div>)
             }}
             </ColorContext.Consumer>
